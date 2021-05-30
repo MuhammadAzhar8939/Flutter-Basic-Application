c2cfda707fd2e9f3989bcf205c21b5a9e01840af
Muhammad Azhar <azhar03067104663@gmail.com>
2021-05-30 11:40:36 +0500

    simple login page in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,8 +10,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Text("Name: Muhammad Azhar\nEmail: azhar03067104663@gmail.com"),
+        appBar: AppBar(
+          title: Center(child: Text("LOGIN PAGE")),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 200,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'UserName',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 200,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'PassWord',
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Login"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
